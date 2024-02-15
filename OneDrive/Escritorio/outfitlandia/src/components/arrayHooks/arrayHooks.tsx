ef3b67e3bd1339ffdc2b7aa8f5f426bd6bed5bf8
martin viera <118699912+Vieramaster@users.noreply.github.com>
2024-02-15 20:33:00 -0300

    adds img

--- OneDrive/Escritorio/outfitlandia/src/components/arrayHooks/arrayHooks.tsx
+++ OneDrive/Escritorio/outfitlandia/src/components/arrayHooks/arrayHooks.tsx
@@ -8,6 +8,11 @@ export type newColors ={
   hue: string;
 }
 
+export type colorData ={
+
+}
+
+
 export type dataJsonTypes ={
   garment: string;
   name:string;
@@ -16,7 +21,7 @@ export type dataJsonTypes ={
   css:string;
   style:string[];
   weather:string[];
-  colors:string[] 
+  colors:colorData[] 
   }
   
   
@@ -43,168 +48,167 @@ export const dataColor =[
     "colorName": "purple",
     "hex": "#603085",
     "title": "purpura",
-    "hue": "hue-rotate(278deg) brightness(80%)"
   },
   {
     "colorName": "lilac",
     "hex": "#C8A2C8",
     "title": "lila",
-    "hue": "hola"
+    
   },
   {
     "colorName": "aero",
     "hex": "#53718d",
     "title": "aero",
-    "hue": "hola"
+    
   },
   {
     "colorName": "jean",
     "hex": "#132b59",
     "title": "jean",
-    "hue": "hola"
+    
   },
   {
     "colorName": "navy blue",
     "hex": "#202a44",
     "title": "azul navy",
-    "hue": "hola"
+    
   },
   {
     "colorName": "french blue",
     "hex": "#318CE7",
     "title": "azul francia (2°)",
-    "hue": "hola"
+    
   },
   {
     "colorName": "skyblue",
     "hex": "#c9e9fc",
     "title": "celeste",
-    "hue": "hola"
+    
   },
   {
     "colorName": "green cake",
     "hex": "#c6e5b1",
     "title":"verde pastel",
-    "hue": "hola"
+    
   },
   {
     "colorName": "aquamarine",
     "hex": "#9fd5d1 ",
     "title": "aguamarina",
-    "hue": "hola"
+    
   },
   {
     "colorName": "dark esmerald",
     "hex": "#08554c",
     "title": "esmeralda oscuro",
-    "hue": "hola"
+    
   },
   {
     "colorName": "dark green",
     "hex": "#363d10",
     "title": "verde oscuro",
-    "hue": "hola"
+    
   },
   {
     "colorName": "army green",
     "hex": "#899478",
     "title": "verde militar",
-    "hue": "hola"
+    
   },
 
   {
     "colorName": "ivory",
     "hex": "#e1dfd1",
     "title": "marfil",
-    "hue": "hola"
+    
   },
 
   {
     "colorName": "yellow",
     "hex": "#ffe759",
     "title": "amarillo",
-    "hue": "hola"
+    
   },
 
   {
     "colorName": "mustard",
     "hex": "#b28024",
     "title": "mostaza",
-    "hue": "hola"
+    
   },
   {
     "colorName": "orange",
     "hex": "#ff8c00",
     "title": "naranja",
-    "hue": "hola"
+    
   },
   {
     "colorName": "red brick",
     "hex": "#a94c2b",
     "title": "ladrillo",
-    "hue": "hola"
+    
   },
   {
     "colorName": "windsor tan",
     "hex": "#8B4513",
     "title": "suela",
-    "hue": "hola"
+    
   },
   {
     "colorName": "chocolate",
     "hex": "#45322e",
     "title": "chocolate",
-    "hue": "hola"
+    
   },
   {
     "colorName": "peanut",
     "hex": "#766142",
     "title": "cacahuate",
-    "hue": "hola"
+    
   },
   
   {
     "colorName": "camel",
     "hex": "#c19a6b",
     "title": "camel",
-    "hue": "hola"
+    
   },
   {
     "colorName": "beige",
     "hex": "#d1bc8a",
     "title": "beige",
-    "hue": "hola"
+    
   },
   {
     "colorName": "Champagne",
     "hex": "#edd6b4",
     "title": "Champagne",
-    "hue": "hola"
+    
     
   },
   {
     "colorName": "sand",
     "hex": "#FFE4B5",
     "title": "arena",
-    "hue": "hola"
+    
   },
   {
     "colorName": "white",
     "hex": "#ffffff",
     "title": "blanco",
-    "hue": "hola"
+    
   },
   {
     "colorName": "light grey",
     "hex": "#c7c8ca",
     "title": "gris claro",
-    "hue": "hola"
+    
   },
   {
     "colorName": "dark grey",
     "hex": "#48494a",
     "title": "gris topo",
-    "hue": "hola"
+    
   },
   {
     "colorName": "worn black",
@@ -216,41 +220,45 @@ export const dataColor =[
     "colorName": "black",
     "hex": "#000000",
     "title": "negro",
-    "hue": "hola"
+    
   },
   {
     "colorName": "burgundy",
     "hex": "#5e041b",
     "title": "borgoña",
-    "hue": "hola"
+    
   },
   {
     "colorName": "red",
     "hex": "#cf010b",
     "title": "rojo",
-    "hue": "hola"
+    
   },
   {
     "colorName": "baby pink",
     "hex": "#f8c8dc",
     "title": "rosa bebe",
-    "hue": "hola"
+    
   },
   {
     "colorName": "old pink",
     "hex": "#a4767a",
     "title": "rosa viejo",
-    "hue": "hola"
+    
   },
   {
     "colorName": "salmon",
     "hex": "#f3a399",
     "title": "salmón",
-    "hue": "hola"
+    
   }
 ]
-    {
-      "colorName":"",
-      "imageColor":""
-    },
+
+
+{
+  "colorName":"",
+  "imageColor": ""
+},
+
+
 
