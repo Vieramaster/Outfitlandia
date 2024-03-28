572b2c80290c474c5cf04853d85e2a223ab95214
Lou Cyx <git@lou.cx>
2024-03-28 14:04:36 -0500

    🏗️ update main.jsx
    Los cambios en `main.jsx`:
    
    1. Importamos solo lo que necesitamos de `react` y `react-dom`.
    2. Usamos `querySelector` en lugar del viejo `getElementById`.

--- OneDrive/Escritorio/outfitlandiaPRO/src/main.jsx
+++ OneDrive/Escritorio/outfitlandiaPRO/src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Header from "../src/Components/Header/Header";
-import HomePage from "../src/Components/HomePage/HomePage";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Header from "./Components/Header/Header";
+import HomePage from "./Components/HomePage/HomePage";
 import "./index.css";
 
-ReactDOM.createRoot(
-  /** @type {HTMLDivElement} */ (document.getElementById("root")),
+createRoot(
+  /** @type {HTMLDivElement} */ (document.querySelector("#root")),
 ).render(
-  <React.StrictMode>
+  <StrictMode>
     <Header />
     <HomePage />
-  </React.StrictMode>,
+  </StrictMode>,
 );
