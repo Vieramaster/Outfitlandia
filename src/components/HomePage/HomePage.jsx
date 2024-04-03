ebd0ecd65c8b5a544cc13ef570afb30537002f8a
vieramaster <vieramaster@hotmail.com>
2024-04-03 19:27:43 -0300

    :construction:

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -63,28 +63,7 @@ export default function HomePage() {
   };
 
   const onClickCombine = () => {
-    const partResults = ["top", "pants", "coat"].filter(
-      (elemento) => elemento !== idGarment
-    );
-
-    const garmentFilter = dataJson.filter((item) =>
-      partResults.includes(item.garment)
-    );
-
-    const [{ style: garmentStyle, weather: garmentWeather }] = showClothes;
-
-    const matchingItems = garmentFilter.filter((item) =>
-      item.style.some(
-        (element) =>
-          garmentStyle.includes(element) &&
-          item.weather.some((element) => garmentWeather.includes(element))
-      )
-    );
-
-    const arrayColorResult = dataColor.filter((item) =>
-      item.combine.includes(garmentColor)
-    );
-
+    const partGarments = ["top", "pants", "coat"];
     const randomNumber = (value) => {
       if (value.length === 0) {
         return null;
@@ -93,119 +72,131 @@ export default function HomePage() {
       return value[result];
     };
 
-    const { combine: outfitColor, combineShoes } =
-      randomNumber(arrayColorResult);
+    const filteredObjects = (array, arrayColor) => {
+      return array.flatMap((obj) => {
+        return obj.colors
+          .filter((color) => arrayColor?.includes(color.colorName))
+          .map((color) => {
+            return {
+              garment: obj.garment,
+              name: obj.name,
+              image: color.imageColor,
+              css: obj.css,
+              style: obj.style,
+              weather: obj.weather,
+              color: color,
+            };
+          });
+      });
+    };
 
-    const filteredObjects = matchingItems.map((obj) => {
-      let filteredColors = obj.colors.filter((color) =>
-        outfitColor?.includes(color.colorName)
+    //si se hace click sobre abrigo, superior o pantalones
+    if (partGarments.includes(idGarment)) {
+      const partResults = partGarments.filter((item) => item !== idGarment);
+
+      const garmentFilter = dataJson.filter((item) =>
+        partResults.includes(item.garment)
       );
-      return { ...obj, colors: filteredColors };
-    });
 
-    const filteredMap = filteredObjects.flatMap((item) => {
-      return item.colors.map((color) => {
-        return {
-          garment: item.garment,
-          name: item.name,
-          image: color.imageColor,
-          css: item.css,
-          style: item.style,
-          weather: item.weather,
-          color: color,
-        };
-      });
-    });
+      const [{ style: garmentStyle, weather: garmentWeather }] = showClothes;
 
-    const groupedByStyle = filteredMap.reduce((acc, item) => {
-      item.style.forEach((style) => {
-        if (!acc[style]) {
-          acc[style] = [];
-        }
-        acc[style].push(item);
-      });
-      return acc;
-    }, {});
+      const matchingItems = garmentFilter.filter((item) =>
+        item.style.some(
+          (element) =>
+            garmentStyle.includes(element) &&
+            item.weather.some((element) => garmentWeather.includes(element))
+        )
+      );
 
-    const pickStyle = () => {
-      let selectedItems = [];
-      for (let style of Object.keys(groupedByStyle)) {
-        let styleItems = [];
+      const arrayColorResult = dataColor.filter((item) =>
+        item.combine.includes(garmentColor)
+      );
 
-        for (let garment of partResults) {
-          let items = groupedByStyle[style].filter(
-            (item) => item.garment === garment
-          );
-          if ((items.length = 2)) {
-            let randomItem = randomNumber(items);
-            if (randomItem !== null) {
-              styleItems[garment] = randomItem;
-            } else {
-              // Si no hay suficientes objetos nos lo salteamos
-              styleItems = null;
-              break;
+      const { combine: outfitColor, combineShoes } =
+        randomNumber(arrayColorResult);
+
+      const outfitFilter = filteredObjects(matchingItems, outfitColor);
+
+      const groupedByStyle = outfitFilter.reduce((acc, item) => {
+        item.style.forEach((style) => {
+          if (!acc[style]) {
+            acc[style] = [];
+          }
+          acc[style].push(item);
+        });
+        return acc;
+      }, {});
+
+      const pickStyle = () => {
+        let selectedItems = [];
+        for (let style of Object.keys(groupedByStyle)) {
+          let styleItems = [];
+
+          for (let garment of partResults) {
+            let items = groupedByStyle[style].filter(
+              (item) => item.garment === garment
+            );
+            if ((items.length = 2)) {
+              let randomItem = randomNumber(items);
+              if (randomItem !== null) {
+                styleItems[garment] = randomItem;
+              } else {
+                // Si no hay suficientes objetos nos lo salteamos
+                styleItems = null;
+                break;
+              }
             }
           }
+          //agregamos los definidos
+          if (
+            styleItems !== null &&
+            !Object.values(styleItems).includes(undefined)
+          ) {
+            selectedItems[style] = styleItems;
+          }
         }
-        //agregamos los definidos
-        if (
-          styleItems !== null &&
-          !Object.values(styleItems).includes(undefined)
-        ) {
-          selectedItems[style] = styleItems;
+        // Eliminamos los estilos vacios
+        for (let style in selectedItems) {
+          if (Object.keys(selectedItems[style]).length === 0) {
+            delete selectedItems[style];
+          }
         }
-      }
-      // Eliminamos los estilos vacios
-      for (let style in selectedItems) {
-        if (Object.keys(selectedItems[style]).length === 0) {
-          delete selectedItems[style];
+        return selectedItems;
+      };
+
+      const styles = pickStyle();
+      const keys = Object.keys(styles);
+      const randomKey = randomNumber(keys);
+      const finishClothes = styles[randomKey];
+
+      const findAcc = (attempt = 0) => {
+        const maxAttempts = 15;
+        if (attempt >= maxAttempts) {
+          return null;
         }
-      }
-      return selectedItems;
-    };
-    const styles = pickStyle();
-    const keys = Object.keys(styles);
-    const randomKey = randomNumber(keys);
-    const keysResult = styles[randomKey];
 
-    const findShoes = dataJson.filter((item) => item.garment === "shoes");
-    const findBelt = dataJson.filter((item) => item.garment === "belt");
+        const findShoes = dataJson.filter((item) => item.garment === "shoes");
 
-    const fafa = () => {
-      let searchStyle = findShoes.filter((item) =>
-        item.style.includes(randomKey)
-      );
-
-      
-      let searchWeather = searchStyle.filter((item) =>
-        garmentWeather.every((val) => item.weather.includes(val))
-      );
-    
-      let filtered = searchWeather.map((obj) => {
-        let filteredColors = obj.colors.filter((color) =>
-          combineShoes?.includes(color.colorName)
-        );
-        return { ...obj, colors: filteredColors };
-      });
-      const filteredMap = filtered.flatMap((item) => {
-        return item.colors.map((color) => {
-          return {
-            garment: item.garment,
-            name: item.name,
-            image: color.imageColor,
-            css: item.css,
-            style: item.style,
-            weather: item.weather,
-            color: color,
-          };
+        let styleResult = findShoes.filter((item) => {
+          return item.style.includes(randomKey);
         });
-      });
-      return filteredMap.length > 0 ? filteredMap : null;
-    };
-    
 
-    console.log(fafa());
-    console.log(randomKey)
+        let searchWeather = styleResult.filter((item) =>
+          garmentWeather.every((val) => item.weather.includes(val))
+        );
+
+        let finishShoes = filteredObjects(searchWeather, combineShoes);
+
+        let randomShoes = randomNumber(finishShoes);
+
+        randomShoes ? randomShoes : findAcc(attempt + 1);
+
+        const accesoryColors = randomShoes.color.colorName
+        return accesoryColors
+      };
+
+      console.log(findAcc());
+    }
   };
 
   return (
